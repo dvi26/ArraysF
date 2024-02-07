fd093082fb8004a05f9978313833db0cdad705c3
dvi26 <david.caro@iesnervion.es>
2024-02-07 09:41:17 +0100

    ejs

--- ArraysBidimensionales/src/e5/Main5.java
+++ ArraysBidimensionales/src/e5/Main5.java
@@ -2,37 +2,59 @@ package e5;
 
 import java.util.Random;
 
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+
 public class Main5 {
 	static Random random = new Random();
 
 	public static void main(String[] args) {
-		int sumafila=0;
-		int sumacolumna=0;
-		int sumatotal=0;
-		for (int i = 0; i < Tabla().length; i++) {
+		int sumafila = 0;
+		int sumacolumna = 0;
+		int sumatotal = 0;
+		int Tabla[][] = new int[4][5];
+		Tabla = Tabla();
+		for (int i = 0; i < Tabla.length; i++) {
 			if (i > 0) {
 				System.out.println();
-				sumafila=0;
-				sumacolumna=0;
+				sumatotal = sumatotal + sumafila;
+				sumafila = 0;
+
 			}
-			for (int j = 0; j < Tabla()[i].length; j++) {
-				System.out.print(Tabla()[i][j]);
+
+			for (int j = 0; j < Tabla[i].length; j++) {
+				System.out.print(Tabla[i][j]);
 				System.out.print("\t");
-				if (j==4) {
-					System.out.print("Fila " + i + ": "+ sumafila);
+				sumatotal = sumatotal + (Tabla[i][j]);
+				sumafila = sumafila + (Tabla[i][j]);
+				if (j == 4) {
+					System.out.print("Fila " + i + ": " + sumafila);
 				}
-				sumatotal=sumatotal+(Tabla()[i][j]);
-				sumafila=sumafila+(Tabla()[i][j]);
-				sumacolumna=sumacolumna+(Tabla()[i][j]);
+
 			}
 		}
+		System.out.println();
+		for (int i = 0; i <= 5; i++) {
+			if (i > 0) {
+				System.out.print("C" + i + " " + sumacolumna + " ");
+				sumatotal= sumatotal+sumacolumna;
+				sumacolumna = 0;
+			}
+			if (i == 5) {
+				break;
+			}
+			for (int j = 0; j < 4; j++) {
+				sumacolumna = sumacolumna + (Tabla[j][i]);
+
+			}
+		}
+		System.out.print("Total: " + sumatotal);
 	}
 
 	static int[][] Tabla() {
 		int tabla[][] = new int[4][5];
 		for (int i = 0; i < tabla.length; i++)
 			for (int j = 0; j < tabla[0].length; j++) {
-				tabla[i][j] = random.nextInt(999) + 100;
+				tabla[i][j] = random.nextInt(100, 999);
 			}
 		return tabla;
 	}
